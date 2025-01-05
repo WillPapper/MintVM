9bfdf49282b46c1f6e2350998c2f559062ef4834
Will Papper <contact@papper.me>
2025-01-04 19:23:38 -0700

    chore: Remove boilerplate

--- src/sqlite.rs
+++ src/sqlite.rs
@@ -1,9 +1,12 @@
-use rusqlite::{Connection, Result};
+use rusqlite::{Connection, Result, ToSql};
+use rusqlite::types::ToSqlOutput;
+use serde::{Serialize, Deserialize};
 
 #[derive(Debug)]
 struct Transactions {
     id: i32,
     transaction_type: TransactionType,
+    // Signed TX data as bytes
     data: Option<Vec<u8>>,
 }
 
@@ -21,46 +24,29 @@ enum TransactionType {
     SetApprovalForAll,
 }
 
-impl TransactionTypeToSql for TransactionType {
+impl ToSql for TransactionType {
     fn to_sql(&self) -> Result<ToSqlOutput<'_>> {
         Ok(ToSqlOutput::from(self.to_string()))
     }
 }
 
 fn main() -> Result<()> {
+    let conn = initialize_db()?;
+
+    Ok(())
+}
+
+fn initialize_db() -> Result<(), rusqlite::Error> {
     let conn = Connection::open_in_memory()?;
 
     conn.execute(
-        "CREATE TABLE person (
-            id    INTEGER PRIMARY KEY,
-            name  TEXT NOT NULL,
+        "CREATE TABLE transactions(
+            id    INTEGER PRIMARY KEY AUTOINCREMENT,
+            transaction_type TEXT NOT NULL,
             data  BLOB
         )",
         (), // empty list of parameters.
-    )?;
-    let me = Person {
-        id: 0,
-        name: "Steven".to_string(),
-        data: None,
-    };
-    conn.execute(
-        "INSERT INTO person (name, data) VALUES (?1, ?2)",
-        (&me.name, &me.data),
-    )?;
-
-    let mut stmt = conn.prepare("SELECT id, name, data FROM person")?;
-    let person_iter = stmt.query_map([], |row| {
-        Ok(Person {
-            id: row.get(0)?,
-            name: row.get(1)?,
-            data: row.get(2)?,
-        })
-    })?;
-
-    for person in person_iter {
-        println!("Found person {:?}", person.unwrap());
-    }
-    Ok(())
+    )?
 }
 
 #[cfg(test)]
